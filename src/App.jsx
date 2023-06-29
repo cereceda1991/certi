465ee9da7ccb0f16ae6366ec5973dd8222974ec9
cereceda1991 <cereceda1991@gmail.com>
2023-06-29 15:51:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import Model1 from "./assets/model1.jpeg";
+import Model2 from "./assets/model2.jpeg";
+import Model3 from "./assets/model3.jpeg";
+import Certificate from "./components/Certificate/Certificate";
+import "./App.css";
+
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
+
+const models = [
+  { id: 1, src: Model1 },
+  { id: 2, src: Model2 },
+  { id: 3, src: Model3 },
+];
+
+function App() {
+  const [imgFile, setImgFile] = useState(null);
+  const [imgPreview, setImgPreview] = useState(models[0].src);
+
+
+
+  if (imgFile) {
+    const reader = new FileReader();
+    reader.readAsDataURL(imgFile);
+    reader.onload = () => {
+      setImgPreview(reader.result);
+    };
+  }
+
+  const exportToPDF = () => {
+    const element = document.getElementById("container");
+  
+    html2canvas(element, { scale: 3 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/jpeg", 1.0);
+  
+      // Establecer las dimensiones A4 horizontal
+      const pdfWidth = 297; // Ancho de A4 horizontal en mm
+      const pdfHeight = 210; // Altura de A4 horizontal en mm
+  
+      // Crear el objeto jsPDF con orientación horizontal y tamaño A4
+      const pdf = new jsPDF({
+        orientation: "landscape",
+        unit: "mm",
+        format: [pdfWidth, pdfHeight],
+      });
+  
+      pdf.addImage(imgData, "JPEG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("certificate.pdf");
+    });
+  };
+  
+  const exportToImage = () => {
+    const element = document.getElementById("container");
+
+    html2canvas(element, { scale: 3 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/jpeg", 1.0);
+
+      // Descargar la imagen
+      const link = document.createElement("a");
+      link.href = imgData;
+      link.download = "certificate.jpg";
+      link.click();
+    });
+  };
+  
+   return (
+    <div className="App">
+      <section className="section_left">
+        <input
+          type="file"
+          className="file"
+          onChange={(e) => {
+            const [files] = e.target.files;
+            setImgFile(files);
+          }}
+        />
+        <div className="options">
+          {models.map((model) => (
+            <img
+              key={model.id}
+              src={model.src}
+              alt="certificate"
+              onClick={() => {
+                setImgFile(null);
+                setImgPreview(model.src);
+              }}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="export_to_pdf">
+      <div className="container" id="container">
+        {imgPreview && <img src={imgPreview} alt="img" className="img" />}
+        <Certificate model={imgPreview} />
+      </div>
+      <div className="container_buttons">
+      <button onClick={exportToPDF}>Exportar a PDF</button>
+      <button onClick={exportToImage}>Exportar a imagen</button>
+      </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Certificate/Certificate.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import "./Certificate.css";
+
+
+const Certificate = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    career_type: "",
+    selected_number: "",
+    ceo_name: "",
+    cto_name: "",
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+return (
+    <section className="Certificate">
+      <form className="content" id="content">
+        <label className="content_title">Certificado de Experiencia</label>
+        <label className="content__label-company">No Country</label>
+        <label className="content__label">Certifica a</label>
+        <input
+          type="text"
+          placeholder="JUAN ERNESTO PEREZ MOLLA"
+          name="name"
+          value={inputs.name}
+          onChange={handleChange}
+          className="content__input-name"
+        />
+        <section className="info__section">
+          <p className="content__paragraph">
+            <label>Por haber participado exitosamente como</label>
+            <input
+              type="text"
+              placeholder="UX/UI Designer"
+              name="career_type"
+              className="content__carreer"
+              value={inputs.career_type}
+              onChange={handleChange}
+            />
+            <label>en la instancia de emulación del Seleccionado</label>
+            <input
+              type="text"
+              placeholder="9"
+              className="content__instance"
+              name="selected_number"
+              value={inputs.selected_number}
+              onChange={handleChange}
+            />
+            <label>llevando a cabo su proyecto en tiempo y forma.</label>
+          </p>
+        </section>
+
+        <div className="director__presidente">
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="Leandro Buzeta Bernasconi"
+                name="ceo_name"
+                value={inputs.ceo_name}
+                onChange={handleChange}
+                className="content__ceo"
+              />
+            </label>
+            <label className="content__label"><b>Co-Founder & CEO</b></label>
+          </div>
+          <div>
+            <label className="content__label">
+              <input
+                type="text"
+                placeholder="Julio Ignacio Otero"
+                name="cto_name"
+                value={inputs.cto_name}
+                onChange={handleChange}
+                className="content__cto"
+              />
+            </label>
+            <label className="content__label"><b>Co-Founder & CTO</b></label>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Certificate;
